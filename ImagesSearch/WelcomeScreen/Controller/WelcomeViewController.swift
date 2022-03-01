b0195f32085097d155a549b6d96b732ec87a68b3
JJefro <jevjefro@gmail.com>
2022-03-01 10:05:15 +0200

    add textFieldsView

--- ImagesSearch/WelcomeScreen/Controller/WelcomeViewController.swift
+++ ImagesSearch/WelcomeScreen/Controller/WelcomeViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configure()
         navigationItem.title = "Welcome"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+}
+
+private extension WelcomeViewController {
+    func configure() {
+        setBackgroundImage()
+    }
+
+    func setBackgroundImage() {
+        let backgroundImageView = UIImageView()
+        backgroundImageView.image = R.image.welcomeBackgroundImage()
+        backgroundImageView.alpha = 0.5
+        backgroundImageView.contentMode = .scaleToFill
+
+        view.insertSubview(backgroundImageView, at: 0)
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
