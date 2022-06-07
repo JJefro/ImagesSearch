00b06ef5306b5e3ba38edd3ea3054a0e5209697b
Jevgenijs <jevjefro@gmail.com>
2022-06-07 17:16:54 +0300

    move settings values

--- ImagesSearch/SearchMediaScreen/Views/SettingsView/Models/SettingsModel.swift
+++ ImagesSearch/SearchMediaScreen/Views/SettingsView/Models/SettingsModel.swift
@@ -12,3 +12,21 @@ struct SettingsModel {
     var selectedItem: String
     var pickerValues: [String]
 }
+
+enum MediaCategory: String, CaseIterable {
+    case all = "All"
+    case photo = "Photo"
+    case illustration = "Illustration"
+    case vector = "Vector"
+}
+
+enum MediaQuality: String, CaseIterable {
+    case small = "Small"
+    case normal = "Normal"
+    case large = "Large"
+}
+
+enum MediaSource: String, CaseIterable {
+    case local = "Local"
+    case remote = "Remote"
+}
